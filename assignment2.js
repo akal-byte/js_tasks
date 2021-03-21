0e8e45008fe9e3a5909863dd70c8dc1956f413e7
akal-bytes <akalerupe@gmail.com>
2021-03-21 19:42:10 +0300

    second commit

---
+++ assignment2.js
@@ -0,0 +1,25 @@
+function getGrade(marks) {
+    var marks=84
+    switch(true){
+        case marks>=80:
+            console.log("A")
+            break;
+         case marks>=70 && marks <79:
+            console.log("B")
+            break;  
+            case marks>=60 &&marks<69:
+                console.log("C")
+                break;
+                case marks>=50 &&marks<59:
+                    console.log("D")
+                    case marks>=0 && marks<49:
+                        console.log("E")
+                        break;
+                        default:
+                            console.log("F")
+
+
+}
+return getGrade
+}
+getGrade()
---
+++ practice.js
@@ -0,0 +1,10 @@
+
+
+function sum(params) {
+    "use strict";
+    a=3;
+    b=2;
+    c=a+b
+    console.log(c)
+}
+sum()
